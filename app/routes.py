99ec2335811bb4c43a7cc9d41917fc95ff97430d
nyxx <nyxx@pop-os.localdomain>
2023-12-07 20:47:25 -0800

    did some basic layingout of home page

---
+++ app/routes.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+import os
+app = Flask(__name__)
+
+@app.route('/home')
+@app.route('/index')
+@app.route('/')
+def home():
+    best = get_best_posts()
+    return render_template('index.html', best_posts=best)
+
+
+def get_best_posts():
+    posts = []
+    for d in os.listdir("static/posts"):
+        if not os.path.exists(f'static/posts/{d}/{d}.jpg'):
+            image = f'static/images/defaultpost.jpg'
+        else:
+            image = f'static/posts/{d}/{d}.jpg'
+        text = open(f'static/posts/{d}/{d}.txt').read()
+        posts.append({"text": text, "image": image})
+
+    return posts
+
+if __name__ == '__main__':
+    app.run(debug=True)
